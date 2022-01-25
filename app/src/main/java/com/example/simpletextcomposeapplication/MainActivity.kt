fab072ca7580ca75c0d48549f9def02c2d131d6d
qloozen <qiangloozen@gmail.com>
2022-01-25 09:19:02 +0100

    Added nested row in a column with different arrangements

--- app/src/main/java/com/example/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/simpletextcomposeapplication/MainActivity.kt
@@ -3,16 +3,12 @@ package com.example.simpletextcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
@@ -32,20 +28,32 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Row {
-            HorizontalColoredRow(Color.Red)
-            HorizontalColoredRow(Color.White)
-            HorizontalColoredRow(Color.Blue)
+        Column(
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ColoredSquare(Color.Red)
+            ColoredSquare(Color.White)
+            ColoredSquare(Color.Blue)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                ColoredSquare(Color.Red)
+                ColoredSquare(Color.White)
+                ColoredSquare(Color.Blue)
+            }
         }
     }
 }
 
 @Composable
-fun HorizontalColoredRow(color: Color) {
+fun ColoredSquare(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
-            .height(600.dp)
+            .height(60.dp)
             .width(60.dp)
     ) {}
 }
