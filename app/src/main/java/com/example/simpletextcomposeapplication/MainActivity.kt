fc0793fdcb64d5400557c876e6ba4f4de740043a
qloozen <qiangloozen@gmail.com>
2022-01-25 08:26:20 +0100

    Reusable Surface composable with input parameter for colors

--- app/src/main/java/com/example/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/simpletextcomposeapplication/MainActivity.kt
@@ -3,36 +3,57 @@ package com.example.simpletextcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleTextComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row {
+            HorizontalColoredRow(Color.Red)
+            HorizontalColoredRow(Color.White)
+            HorizontalColoredRow(Color.Blue)
+        }
+    }
+}
+
+@Composable
+fun HorizontalColoredRow(color: Color) {
+    Surface(
+        color = color,
+        modifier = Modifier
+            .height(600.dp)
+            .width(60.dp)
+    ) {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SimpleTextComposeApplicationTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
