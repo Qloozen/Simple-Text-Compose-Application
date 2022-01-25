f5d9489ddee263b1e4a1bbd9f81913993ec5be74
qloozen <qiangloozen@gmail.com>
2022-01-25 21:14:57 +0100

    Delegated state from branch-3 to a ViewModel

--- app/src/main/java/com/example/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/simpletextcomposeapplication/MainActivity.kt
@@ -1,29 +1,18 @@
 package com.example.simpletextcomposeapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
-
-val greetingList : ArrayList<String> = arrayListOf(
-    "Name1",
-    "Name2",
-    "Name3",
-    "Name4"
-)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,56 +24,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(viewModel: MainViewModel = MainViewModel()) {
     // When states changes will cause recomposition of the composable, so the changes will be displayed for dynamic content.
-    val greetingListState = remember { mutableStateListOf<String>("John")}
-    val newNameStateContent = remember { mutableStateOf("")}
+    val newNameStateContent = viewModel.textFieldState.observeAsState("")
 
     Surface(
         color = Color.White,
         modifier = Modifier.fillMaxSize()
     ) {
-        GreetingList(
-            greetingListState,
-            { greetingListState.add(newNameStateContent.value) },
-            newNameStateContent.value,
-            { newName -> newNameStateContent.value = newName})
+        GreetingMessage(
+            newNameStateContent.value
+        ) { newName -> viewModel.onTextChanged(newName) }
     }
 }
 
 @Composable
-fun GreetingList(greetingList: List<String>,
-                 buttonClick: () -> Unit,
+fun GreetingMessage(
                  textFieldValue: String,
                  textFieldUpdate: (newName: String) -> Unit
 ) {
     Column(
         Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        for(name in greetingList) {
-            Greeting(name = name)
-        }
 
-        // Creating a simple TextField.
-        // 'value' holds the input value stored in the state.
-        // 'onValueChange' is a lambda where the input in the state is getting replaced.
-        // All state changes should not be handled by the composable itself, therefore delegate it to the parent. */
+        Text(text = "TypedText: $textFieldValue" , style = MaterialTheme.typography.h5)
+
         TextField(value = textFieldValue, onValueChange = textFieldUpdate)
-
-        Button(onClick = buttonClick) {
-            Text("Add new name")
-        }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name",
-        style = MaterialTheme.typography.h5
-    )
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/simpletextcomposeapplication/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.simpletextcomposeapplication
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val textFieldState = MutableLiveData("")
+
+    fun onTextChanged(newText: String) {
+        textFieldState.value = newText
+    }
+}
